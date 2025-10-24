bf4a6037a0dcb23b39d235c69124eb20f55ea3e3
Adogo <gonda.ado@gmail.com>
2025-10-24 22:08:56 +0200

    add: ? for safety load

--- frontend/src/routes/farms/$id.tsx
+++ frontend/src/routes/farms/$id.tsx
@@ -25,7 +25,7 @@ function FarmDetailPage() {
   if (isLoading) {
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
-        {Array.from({ length: 6 }).map((_, i) => (
+        {Array.from({ length: 6 })?.map((_, i) => (
           <Card key={i} className="animate-pulse h-40" />
         ))}
         <p className="col-span-full text-center text-gray-500 mt-4">
@@ -43,19 +43,19 @@ function FarmDetailPage() {
 
   return (
     <div className="p-6">
-      <h2 className="text-2xl font-bold mb-2">{farm.name}</h2>
+      <h2 className="text-2xl font-bold mb-2">{farm?.name}</h2>
       <p className="mb-2">{farm.description}</p>
       <p className="mb-2">
         {farm.city}, {farm.street}, {farm.region}, {farm.postalCode},{" "}
         {farm.country}
       </p>
       <p className="mb-4 font-semibold">
-        {t("farmsPage.farmer")}: {farm.farmer.name}
+        {t("farmsPage.farmer")}: {farm.farmer?.name}
       </p>
 
       <h3 className="text-xl font-semibold mb-4">{t("farmsPage.products")}</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {farm.products.map((product) => (
+        {farm.products?.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
