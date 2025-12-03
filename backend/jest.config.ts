26a04db620d6b5b4914ff0f51177de6ab50631a6
Adogo <gonda.ado@gmail.com>
2025-12-03 18:09:00 +0100

    fix: jest types fixes

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
   moduleFileExtensions: ["ts", "js", "json"],
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "./tsconfig.jest.json" }],
+  },
 };
 
 export default config;
